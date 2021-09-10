305249ea118b62fc380281b22da1413411048101
Diego Taipe <diegotaipe@outlook.com>
2021-09-10 16:57:26 -0500

    Tarea 26: ingreso nuevo array

--- src/main/java1/ejercicios/ordenamiento/vectores/Paciente.java
+++ src/main/java1/ejercicios/ordenamiento/vectores/Paciente.java
@@ -5,54 +5,39 @@ public class Paciente implements Comparable<Paciente> {
 	protected String apellido;
 	protected int edad;
 	protected String sintoma;
-	
+
 	public String getNombre() {
 		return nombre;
 	}
-
-
 
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 
-
-
 	public String getApellido() {
 		return apellido;
 	}
-
-
 
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
 	}
 
-
-
 	public int getEdad() {
 		return edad;
 	}
-
-
 
 	public void setEdad(int edad) {
 		this.edad = edad;
 	}
 
-
-
 	public String getSintoma() {
 		return sintoma;
 	}
 
-
-
 	public void setSintoma(String sintoma) {
 		this.sintoma = sintoma;
 	}
-	
-	
+
 	public int compareTo(Paciente o) {
 		// TODO Auto-generated method stub
 		if (this.edad > o.getEdad()) {
@@ -63,9 +48,10 @@ public class Paciente implements Comparable<Paciente> {
 			return 1;
 		}
 	}
+
 	@Override
 	public String toString() {
 		return "Paciente: " + nombre + " - Apellido: " + apellido + " - Edad: " + edad + " - Síntoma: " + sintoma;
 	}
-	
+
 }
