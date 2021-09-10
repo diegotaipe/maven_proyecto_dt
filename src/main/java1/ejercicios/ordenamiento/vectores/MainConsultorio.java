2fc9424141e55d8b0782d93dc6adc6d6f0f1c68b
Diego Taipe <diegotaipe@outlook.com>
2021-09-10 16:45:09 -0500

    Tarea 27: añadir vectores/array

---
+++ src/main/java1/ejercicios/ordenamiento/vectores/MainConsultorio.java
@@ -0,0 +1,69 @@
+package ejercicios.ordenamiento.vectores;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MainConsultorio {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner lectorString = new Scanner(System.in);
+		Scanner lectorInt = new Scanner(System.in);
+		int opcion;
+		int posicion = 0;
+
+		Paciente listadoPacientes[] = new Paciente[0];
+		do {
+			System.out.println();
+			System.out.println("***********************");
+			System.out.println("1. Registrar Paciente ");
+			System.out.println("2. Imprimir reporte ");
+			System.out.println("3. SALIR ");
+			System.out.println("***********************");
+			opcion = lectorInt.nextInt();
+
+			if (opcion == 1) {
+				// se crea array/vector temporal, con tamano +1 del original
+				Paciente listadoTemporal[] = new Paciente[listadoPacientes.length + 1];
+				// se establece el mismo tamano del temporal con el original
+				for (int i = 0; i < listadoPacientes.length; i++) {
+					listadoTemporal[i] = listadoPacientes[i];
+				}
+				System.out.println("***** Registro del paciente *****");
+				System.out.print("Ingrese nombre del paciente: ");
+				String nombre = lectorString.nextLine();
+				System.out.print("Ingrese apellido del paciente: ");
+				String apellido = lectorString.nextLine();
+				System.out.print("Ingrese edad del paciente: ");
+				int edad = lectorInt.nextInt();
+				System.out.print("Ingrese síntoma del paciente: ");
+				String sintoma = lectorString.nextLine();
+
+				Paciente pacienteIngreso = new Paciente();
+				pacienteIngreso.setNombre(nombre);
+				pacienteIngreso.setApellido(apellido);
+				pacienteIngreso.setEdad(edad);
+				pacienteIngreso.setSintoma(sintoma);
+				// rellenado del vector temporal
+				listadoTemporal[posicion] = pacienteIngreso;
+				// los valores del temporal van al original
+				listadoPacientes = listadoTemporal;
+				posicion++;
+
+				System.out.println(Arrays.toString(listadoPacientes));
+			}
+			if (opcion == 2) {
+				System.out.println("***** Reporte *****");
+				Arrays.sort(listadoPacientes);
+				for (int i = 0; i < listadoPacientes.length; i++) {
+
+					System.out.println(listadoPacientes[i]);
+
+				}
+			}
+		} while (opcion != 3);
+		System.out.println("Gracias por usar el sistema");
+		// System.out.println("listado final de estudiantes");
+		// System.out.println(Arrays.toString(listadoPacientes));
+	}
+}
---
+++ src/main/java1/ejercicios/ordenamiento/vectores/Paciente.java
@@ -0,0 +1,73 @@
+package ejercicios.ordenamiento.vectores;
+
+public class Paciente implements Comparable<Paciente> {
+	protected String nombre;
+	protected String apellido;
+	protected int edad;
+	protected String sintoma;
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+
+	public String getApellido() {
+		return apellido;
+	}
+
+
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+
+
+	public int getEdad() {
+		return edad;
+	}
+
+
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+
+
+	public String getSintoma() {
+		return sintoma;
+	}
+
+
+
+	public void setSintoma(String sintoma) {
+		this.sintoma = sintoma;
+	}
+	
+	
+	public int compareTo(Paciente o) {
+		// TODO Auto-generated method stub
+		if (this.edad > o.getEdad()) {
+			return -1;
+		} else if (this.edad == o.getEdad()) {
+			return 0;
+		} else {
+			return 1;
+		}
+	}
+
+
+	@Override
+	public String toString() {
+		return "Paciente: " + nombre + " - Apellido: " + apellido + " - Edad: " + edad + " - Síntoma: " + sintoma;
+	}
+	
+}
