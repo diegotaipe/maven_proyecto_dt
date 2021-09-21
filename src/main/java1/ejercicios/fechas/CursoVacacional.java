36918599900f3c4a8ab0824a7021e81d0ded15f4
Diego Taipe <diegotaipe@outlook.com>
2021-09-20 20:05:36 -0500

    Taller en clase 22: variable de tipo clase

--- src/main/java1/ejercicios/fechas/CursoVacacional.java
+++ src/main/java1/ejercicios/fechas/CursoVacacional.java
@@ -6,25 +6,40 @@ public class CursoVacacional {
 	private String nombre;
 	private LocalDate fechaInicio;
 	private LocalDate fechaFin;
-	
-	//metodos SET  y GET
+	// agupo la clase instructor como objeto
+	private Instructor instructor; // Variable de tipo clase
+
+	// metodos SET y GET
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	public LocalDate getFechaInicio() {
 		return fechaInicio;
 	}
+
 	public void setFechaInicio(LocalDate fechaInicio) {
 		this.fechaInicio = fechaInicio;
 	}
+
 	public LocalDate getFechaFin() {
 		return fechaFin;
 	}
+
 	public void setFechaFin(LocalDate fechaFin) {
 		this.fechaFin = fechaFin;
 	}
-	
+
+	public Instructor getInstructor() {
+		return instructor;
+	}
+
+	public void setInstructor(Instructor instructor) {
+		this.instructor = instructor;
+	}
+
 }
